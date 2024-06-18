8a1b768f8a2d92a46cca886e1904c77411af3109
TraxionRPh <griffts@comcast.net>
2024-06-17 20:44:39 -0400

    chore: Refactor drop_calculator_tab UI for better organization and readability

--- drop_calculator_tab.py
+++ drop_calculator_tab.py
@@ -1,42 +1,54 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLabel, QLineEdit, QComboBox, QPushButton
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QLabel, QLineEdit, QComboBox, QPushButton, QGroupBox, QHBoxLayout
 from PyQt6.QtGui import QIntValidator, QDoubleValidator
+from PyQt6.QtCore import Qt
 
 def create_drop_calculator_tab(parent):
     drop_calculator_tab = QWidget()
-    layout = QVBoxLayout()
+    main_layout = QVBoxLayout()
+    main_layout.setContentsMargins(15, 15, 15, 15)
+
+    input_group_box = QGroupBox()
+    input_layout = QGridLayout()
+    input_layout.setSpacing(10)
 
     grid = QGridLayout()
     grid.setSpacing(10)
     
-    grid.addWidget(QLabel("Quantity (ml):"), 0, 0)
+    input_layout.addWidget(QLabel("Quantity (ml):"), 0, 0, Qt.AlignmentFlag.AlignRight)
     parent.quantity_edit = QLineEdit()
     parent.quantity_edit.setValidator(QDoubleValidator(0.0, 10000.0, 2))
-    grid.addWidget(parent.quantity_edit, 0, 1)
+    input_layout.addWidget(parent.quantity_edit, 0, 1)
 
-    grid.addWidget(QLabel("Drops per Dose:"), 1, 0)
+    input_layout.addWidget(QLabel("Drops per Dose:"), 1, 0, Qt.AlignmentFlag.AlignRight)
     parent.drops_per_dose_edit = QLineEdit()
     parent.drops_per_dose_edit.setValidator(QIntValidator(1, 100))
-    grid.addWidget(parent.drops_per_dose_edit, 1, 1)
+    input_layout.addWidget(parent.drops_per_dose_edit, 1, 1)
 
-    grid.addWidget(QLabel("Doses per Day:"), 2, 0)
+    input_layout.addWidget(QLabel("Doses per Day:"), 2, 0, Qt.AlignmentFlag.AlignRight)
     parent.doses_per_day_edit = QLineEdit()
     parent.doses_per_day_edit.setValidator(QIntValidator(1, 100))
-    grid.addWidget(parent.doses_per_day_edit, 2, 1)
+    input_layout.addWidget(parent.doses_per_day_edit, 2, 1)
 
-    grid.addWidget(QLabel("Which Eye/Ear:"), 3, 0)
+    input_layout.addWidget(QLabel("Which Eye/Ear:"), 3, 0, Qt.AlignmentFlag.AlignRight)
     parent.site_combo = QComboBox()
-    parent.site_combo.addItems(["Right Eye/Ear", "Left Eye/Ear", "Both Eyes/Ears"])
-    grid.addWidget(parent.site_combo, 3, 1)
+    parent.site_combo.addItems(["Right", "Left", "Both"])
+    input_layout.addWidget(parent.site_combo, 3, 1)
 
+    input_group_box.setLayout(input_layout)
+
+    button_layout = QHBoxLayout()
     parent.calculate_btn = QPushButton("Calculate Days Supply")
     parent.calculate_btn.clicked.connect(lambda: calculate_days_supply(parent))
-    grid.addWidget(parent.calculate_btn, 4, 0, 1, 2)
+    button_layout.addWidget(parent.calculate_btn, alignment=Qt.AlignmentFlag.AlignCenter)
 
     parent.days_supply_result = QLabel("")
-    layout.addWidget(parent.days_supply_result)
+    parent.days_supply_result.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    button_layout.addWidget(parent.days_supply_result)
 
-    layout.addLayout(grid)
-    drop_calculator_tab.setLayout(layout)
+    main_layout.addWidget(input_group_box)
+    main_layout.addLayout(button_layout)
+
+    drop_calculator_tab.setLayout(main_layout)
     drop_calculator_tab.setObjectName("Drops Days Supply Calculator")
     parent.tabs.addTab(drop_calculator_tab, "Drops Days Supply Calculator")
 
@@ -47,7 +59,7 @@ def calculate_days_supply(parent):
         doses_per_day = int(parent.doses_per_day_edit.text())
         site = parent.site_combo.currentText()
 
-        multiplier = 1 if site in ["Right Eye/Ear", "Left Eye/Ear"] else 2
+        multiplier = 1 if site in ["Right", "Left"] else 2
 
         total_doses = quantity * 20 / drops_per_dose
         days_supply = total_doses / (doses_per_day * multiplier)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget
+from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtCore import QSettings
 from date_difference_tab import create_date_difference_tab, calculate_date_difference
 from fillable_date_tab import create_fillable_tab, calculate_date_addition
@@ -12,6 +13,7 @@ class PharmacistCalculator(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Pharmacist Calculator")
+        self.setWindowIcon(QIcon("C:/Users/Griff/OneDrive/Documents/PythonProjects/PharmCalc/icon.png"))
         self.settings = QSettings("TraxionRPh", "PharmacistCalculator")
         self.load_window_geometry()
 
@@ -19,8 +21,41 @@ class PharmacistCalculator(QMainWindow):
         self.tabs.setMovable(True)
         self.setCentralWidget(self.tabs)
 
+        self.available_tabs = {
+            "Date Difference": create_date_difference_tab,
+            "Fillable Date": create_fillable_tab,
+            "Accumulation Calculator": create_accumulation_tab,
+            "Dosing": create_dosing_tab,
+            "Strength Conversion": create_strength_conversion_tab,
+            "Drops Days Supply Calculator": create_drop_calculator_tab,
+        }
+
+        self.create_menu_bar()
         self.load_tab_order()
         self.apply_stylesheet()
+
+    def create_menu_bar(self):
+        menu_bar = self.menuBar()
+        view_menu = menu_bar.addMenu("View")
+
+        self.tab_actions = {}
+        for tab_name in self.available_tabs.keys():
+            action = QAction(tab_name, self, checkable=True)
+            action.setChecked(True)
+            action.triggered.connect(self.toggle_tab)
+            view_menu.addAction(action)
+            self.tab_actions[tab_name] = action
+    
+    def toggle_tab(self):
+        action = self.sender()
+        tab_name = action.text()
+
+        if action.isChecked():
+            self.available_tabs[tab_name](self)
+        else:
+            index = self.tabs.indexOf(self.tabs.findChild(QWidget, tab_name))
+            if index != -1:
+                self.tabs.removeTab(index)
     
     def load_window_geometry(self):
         if self.settings.contains("geometry"):
@@ -30,57 +65,31 @@ class PharmacistCalculator(QMainWindow):
     
     def save_window_geometry(self):
         self.settings.setValue("geometry", self.saveGeometry())
-
-    '''def load_tab_order(self):
-        try:
-            tab_order = self.settings.value("tab_order")
-            for tab_name in tab_order:
-                if tab_name == "Date Difference":
-                    create_date_difference_tab(self)
-                elif tab_name == "Fillable Date":
-                    create_fillable_tab(self)
-                elif tab_name == "Accumulation Calculator":
-                    create_accumulation_tab(self)
-                elif tab_name == "Dosing":
-                    create_dosing_tab(self)
-                elif tab_name == "Strength Conversion":
-                    create_strength_conversion_tab(self)
-                elif tab_name == "Drops Days Supply Calculator":
-                    create_drop_calculator_tab(self)
-        except TypeError:
-            create_date_difference_tab(self)
-            create_fillable_tab(self)
-            create_accumulation_tab(self)
-            create_dosing_tab(self)
-            create_strength_conversion_tab(self)
-            create_drop_calculator_tab(self)'''
     
     def load_tab_order(self):
-        available_tabs = {
-            "Date Difference": create_date_difference_tab,
-            "Fillable Date": create_fillable_tab,
-            "Accumulation Calculator": create_accumulation_tab,
-            "Dosing": create_dosing_tab,
-            "Strength Conversion": create_strength_conversion_tab,
-            "Drops Days Supply Calculator": create_drop_calculator_tab,
-        }
-
         try:
             tab_order = self.settings.value("tab_order", [])
-
             loaded_tabs = set()
 
             for tab_name in tab_order:
-                if tab_name in available_tabs:
-                    available_tabs[tab_name](self)
+                if tab_name in self.available_tabs:
+                    self.available_tabs[tab_name](self)
                     loaded_tabs.add(tab_name)
+                    self.tab_actions[tab_name].setChecked(True)
+                else:
+                    self.tab_actions[tab_name].setChecked(False)
             
-            for tab_name, create_function in available_tabs.items():
+            for tab_name, create_function in self.available_tabs.items():
                 if tab_name not in loaded_tabs:
-                    create_function(self)
+                    visible = self.settings.value(f"tab_visible_{tab_name}", True, type=bool)
+                    if visible:
+                        create_function(self)
+                        self.tab_actions[tab_name].setChecked(True)
+                    else:
+                        self.tab_actions[tab_name].setChecked(False)
         
         except TypeError:
-            for create_function in available_tabs.values():
+            for create_function in self.available_tabs.values():
                 create_function(self)
 
     def save_tab_order(self):
@@ -112,6 +121,8 @@ class PharmacistCalculator(QMainWindow):
     def closeEvent(self, event):
         self.save_tab_order()
         self.save_window_geometry()
+        for tab_name, action in self.tab_actions.items():
+            self.settings.setValue(f"tab_visible_{tab_name}", action.isChecked())
         event.accept()
 
     def calculate_date_difference(self):
