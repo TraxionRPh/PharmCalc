c2238be56ed2b2370ebe2696b12e16c1fef663ec
TraxionRPh <griffts@comcast.net>
2024-06-20 20:04:48 -0400

    chore: Update URLs for version check and update installer

--- update.py
+++ update.py
@@ -3,8 +3,8 @@ import os
 import subprocess
 
 APP_VERSION = '1.2'
-VERSION_CHECK_URL = 'https://raw.githubusercontent.com/TraxionRPh/PharmCalc/main/CurrentVersion/CURRENT_VERSION.txt'
-UPDATE_INSTALLER_URL = 'https://raw.githubusercontent.com/TraxionRPh/PharmCalc/main/CurrentVersion/PharmCalcInstaller.exe'
+VERSION_CHECK_URL = 'https://raw.githubusercontent.com/TraxionRPh/PharmCalc/main/CurrentVersion/CURRENT_VERSION.txt?token=$(date%20+%s)'
+UPDATE_INSTALLER_URL = 'https://raw.githubusercontent.com/TraxionRPh/PharmCalc/main/CurrentVersion/PharmCalcInstaller.exe?token=$(date%20+%s)'
 UPDATE_DOWNLOAD_DIR = './downloads'
 INSTALLER_PATH = os.path.join(UPDATE_DOWNLOAD_DIR, 'PharmCalcInstaller.exe')
 
